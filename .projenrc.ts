aeeda99ea5b0350c37b4c383192f9075644a3389
Dominik Jakielski <git@jakielski.de>
2023-09-03 11:58:09 +0200

    not working jsii

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,38 +1,52 @@
 import { cdktf } from "projen";
 import { GitlabConfiguration } from "projen/lib/gitlab";
-import { NodePackageManager, UpdateSnapshot } from "projen/lib/javascript";
+import {
+  NodePackageManager,
+  UpdateSnapshot,
+  UpgradeDependenciesSchedule,
+} from "projen/lib/javascript";
 import { ReleaseTrigger } from "projen/lib/release";
 
 const project = new cdktf.ConstructLibraryCdktf({
+  name: "cdktf-nodejs-function",
+  description: "construct library for nodejs lambda function",
   author: "Dominik Jakielski",
   authorAddress: "git@jakielski.de",
   cdktfVersion: "^0.18.0",
   defaultReleaseBranch: "main",
-  jsiiVersion: "~5.0.0",
-  name: "cdktf-nodejs-function",
+  jsiiVersion: "^5.0.21",
   projenrcTs: true,
   repositoryUrl:
     "https://gitlab.com/dj-cdktf-libraries/cdktf-nodejs-function.git",
   releaseToNpm: true,
   github: true,
+  dependabot: true,
+  minMajorVersion: 1,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ["auto-approve", "automerge", "dependencies"],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  prettier: true,
   githubOptions: {
     workflows: true,
   },
-  eslintOptions: {
-    dirs: [],
-    prettier: true,
-  },
-  pnpmVersion: "8",
-  devDeps: ["esbuild@^0.19.2", "testcontainers"],
+  docgen: true,
+  minNodeVersion: "18.0.0",
+  deps: ["esbuild@^0.19.2"],
+  devDeps: ["testcontainers"],
+  peerDeps: ["@cdktf/provider-aws@^17.0.2"],
+  bundledDeps: ["esbuild@^0.19.2"],
   jestOptions: {
     updateSnapshot: UpdateSnapshot.NEVER,
   },
-  description: "construct library for nodejs lambda function",
   release: true,
   releaseTrigger: ReleaseTrigger.continuous(),
   packageManager: NodePackageManager.PNPM,
+  keywords: ["ckd", "aws", "lambda", "nodejs", "constructs"],
 });
-project.addPeerDeps("@cdktf/provider-aws@12.x");
+
 new GitlabConfiguration(project, {
   stages: ["test", "release"],
   default: {
